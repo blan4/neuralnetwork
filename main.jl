e5ec5051c1f231de36e083151dc2fdac90cce1bc
Ilya Siganov <ilya.blan4@gmail.com>
2016-07-23 12:25:29 +0600

    update run script

--- main.jl
+++ main.jl
@@ -1,9 +1,10 @@
 include("neural_network.jl")
 
 importall NeuralNetwork
+using JLD
 
-function read_data(path)
-  data = readcsv("train.csv")
+function load_train(path)
+  data = readcsv(path)
   train_data = Batch[]
   for i in 2:size(data)[1]
     l = data[i,1]
@@ -14,29 +15,48 @@ function read_data(path)
   train_data
 end
 
-function mnist_test()
+function load_test(path)
+  data = readcsv(path)
+  test_data = Array{Float64,2}[]
+  for i in 2:size(data)[1]
+    input = [d / 255 for d in data[i,1:end]]'
+    push!(test_data, input')
+  end
+  test_data
+end
+
+function train()
+  nd = NetworkData([784, 100, 10])
   println("Load data")
-  train_data = read_data("train.csv")
-  nd = NetworkData([784, 30, 10])
+  train_data = load_train("data/train.csv")
   println("Start training")
-  train!(nd, train_data[1:30000], 1000, 10, 3.0, train_data[30001:end], ((got, expected) -> indmax(got) == indmax(expected)))
+  train!(nd, train_data, 100, 10, 3.0, train_data, ((got, expected) -> indmax(got) == indmax(expected)))
   println("Finish training")
+  println("Saving")
+  jldopen("data/nd.jld", "w") do file
+    addrequire(file, NeuralNetwork)
+    write(file, "nd", nd)
+  end
+  println("Saved")
+  nd
 end
 
-mnist_test()
-
-
-
-########
-# TEST #
-########
-
-function xorTest()
-  nd = NetworkData([2,4,1])
-  data = [Batch([0.0 0.0]', [0.0]'), Batch([1.0 1.0]', [0.0]'), Batch([1.0 0.0]', [1.0]'), Batch([0.0 1.0]', [1.0]')]
-  println("Start training")
-  train!(nd, data, 1000, 2, 0.1, data, ((got, expected) -> round(got) == expected))
-  println("Finish training")
+function test(nd::NetworkData)
+  println("Load test")
+  test_data = load_test("data/test.csv")
+  println("loaded")
+  labels = Int[]
+  println("Testing")
+  for i in 1:length(test_data)
+    l = feed_forward(nd, test_data[i])
+    push!(labels, indmax(l)-1)
+  end
+  println("Finish")
+  indexes = [i for i in 1:length(labels)]
+  writecsv("data/submission.csv", [indexes labels])
+  [indexes labels]
 end
 
-# xorTest()
+nd = train()
+#nd::NetworkData = load("data/nd.jld", "nd")
+test(nd)
